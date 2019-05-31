99cea1a877c5fdb977fc7c1ad8aa2b8b8d332334
Vilson Júnior <vilsonjrcorrea@gmail.com>
2019-05-31 05:20:01 -0300

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,6 @@ import {
   FormMember,
   ViewMember
 } from './pages';
-// import { isAuthenticated } from './services/auth';
-
-// const PrivateRoute = ({ component: Component, ...rest }) => (
-//   <Route
-//     {...rest}
-//     render={props =>
-//       isAuthenticated() ? (
-//         <Component {...props} />
-//       ) : (
-//         <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-//       )
-//     }
-//   />
-// );
 
 export default () => (
   <BrowserRouter>
