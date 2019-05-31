7b14efba7d53dad2e9ed348447af77ba30cbdcce
Vilson Júnior <vilsonjrcorrea@gmail.com>
2019-05-31 05:22:05 -0300

    form member

--- src/pages/Member/FormMember.jsx
+++ src/pages/Member/FormMember.jsx
@@ -1,38 +1,39 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/destructuring-assignment */
-/* eslint-disable prettier/prettier */
 import React from "react";
+import { toast } from "react-toastify";
+import Spinner from "react-bootstrap/Spinner";
 import Joi from "joi-browser";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Input from "../../components/common/form/Input";
-import { getMember } from "../../services/memberService";
+import { getMember, saveMember } from "../../services/memberService";
 import cleanMember from "../../utils/objects/member";
 import getSchema from "../../validators/member";
 import Dashboard from '../../components/Layout/Dashboard';
 
 class FormMember extends React.Component {
-
-  state:{
-    data:{
-      member:cleanMember,
-    },
-    errors: {}
-  };
-
   schema = getSchema();
 
-  async componentDidMount(){
-    await this.init();
+  async componentDidMount() {
+    await this.initState();
   }
 
-  doSubmit = event => {
+  handleSubmit = async event => {
     try {
       event.preventDefault();
-      console.log(this.state.member);
+      const { member } = this.state.data;
+      const { status } = await saveMember(member);
+      if (status === 200) {
+        toast.success("Salvo com sucesso!");
+        this.props.history.push("/member");
+      }else{
+        toast.error("Erro ao salvar!");
+      }
     } catch (e) {
       if (e.response && e.response.status === 400) {
-        // eslint-disable-next-line react/no-access-state-in-setstate
         const errors = { ...this.state.errors };
         errors.username = e.response.data;
         this.setState({ errors });
@@ -45,9 +46,7 @@ class FormMember extends React.Component {
     const errorMessage = this.validateProperty(input);
     if (errorMessage) errors[input.name] = errorMessage;
     else delete errors[input.name];
-    console.log(this.state);
-    // eslint-disable-next-line react/no-access-state-in-setstate
-    const member = { ...this.state.member };
+    const { member } = { ...this.state.data };
     member[input.name] = input.value;
     this.setState({ member, errors });
   };
@@ -59,88 +58,93 @@ class FormMember extends React.Component {
     return error ? error.details[0].message : null;
   };
 
-  async init(){
-    try{
-      // eslint-disable-next-line react/destructuring-assignment
-      const {id}= this.props.match.params;
-      if(id==='new')return;
-      const {data:saveMember}=await getMember(id);
-      this.setState({member:saveMember});
-    }catch(e){
+  validateForm = () => {
+    const options = { abortEarly: false };
+    const { member } = { ...this.state.data };
+    delete member.questionario;
+    delete member.atividades;
+    delete member.mensalidades;
+    const { error } = Joi.validate(member, this.schema, options);
+    if (!error) return null;
+    const errors = {};
+    for (const item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  async initState() {
+    try {
+      const { id } = this.props.match.params;
+      if (id === 'new') {
+        this.setState({ data: { member: cleanMember }, errors: {} });
+      } else {
+        const { data: saveMember } = await getMember(id);
+        this.setState({ data: { member: saveMember }, errors: {} });
+      }
+    } catch (e) {
       console.log(e);
     }
   }
 
-  renderMemberDisabled=member=>{
-    return (
-      <div>
-        <Col md={{ span: 12, offset: 0 }}>
-          <Card.Body>
-            {/* <form onSubmit={this.handleSubmit}>
-              {this.renderDisabledInput("nome", "Nome", member.nome)}
-              {this.renderDisabledInput("email", "E-mail", member.email)}
-              <Row>
-                <Col>{this.renderDisabledInput("fone", "Telefone", member.fone)}</Col>
-                <Col>{this.renderDisabledInput("celular", "Celular", member.celular)} </Col>
-              </Row>
-              <Row>
-                <Col>{this.renderDisabledInput("dataNascimento", "Data nascimento", member.dataNascimento)}</Col>
-                <Col>{this.renderDisabledInput("sexo", "Sexo", member.sexo)}</Col>
-              </Row>
-              <Row>
-                <Col>{this.renderDisabledInput("cidade", "Cidade", member.cidade)}</Col>
-                <Col>{this.renderDisabledInput("uf", "UF", member.uf)}</Col>
-                <Col>{this.renderDisabledInput("cep", "CEP", member.cep)}</Col>
-              </Row>
-              <Row>
-                <Col>{this.renderDisabledInput("rua", "Rua", member.rua)}</Col>
-                <Col>{this.renderDisabledInput("numero", "Número", member.numero)}</Col>
-                <Col>{this.renderDisabledInput("bairro", "Bairro", member.bairro)}</Col>
-              </Row>
-            </form> */}
-          </Card.Body>
-        </Col>
-      </div>
-    );
-  }
-
-  renderMemberEnabled=()=>{
-    console.log(this.state);
+  renderMemberEnabled = () => {
+    const { member } = this.state.data;
+    const { errors } = this.state;
     return (
       <div>
         <Col md={{ span: 12, offset: 0 }}>
           <Card.Body>
             <form onSubmit={this.handleSubmit}>
-              <Input
-                name="nome"
-                type="text"
-                value={this.state.member.nome}
-                label="Nome"
-                onChange={this.handleChange}
-                error={this.state.errors.name}
-              />
-              {/* {this.renderInput("nome", "Nome",this.handleChange)}
-              {this.renderInput("email", "E-mail",this.handleChange)}
+              <Input name="nome" type="text" value={member.nome} error={errors.nome} onChange={this.handleChange} label="Nome" />
+              <Input name="email" type="text" value={member.email} error={errors.email} onChange={this.handleChange} label="E-mail" />
               <Row>
-                <Col>{this.renderInput("fone", "Telefone",this.handleChange)}</Col>
-                <Col>{this.renderInput("celular", "Celular",this.handleChange)} </Col>
+                <Col>
+                  <Input name="fone" type="text" value={member.fone} error={errors.fone} onChange={this.handleChange} label="Telefone" />
+                </Col>
+                <Col>
+                  <Input name="celular" type="text" value={member.celular} error={errors.celular} onChange={this.handleChange} label="Celular" />
+                </Col>
               </Row>
               <Row>
-                <Col>{this.renderInput("dataNascimento", "Data nascimento",this.handleChange)}</Col>
-                <Col>{this.renderInput("sexo", "Sexo")}</Col>
+                <Col>
+                  <Input name="dataNascimento" type="text" value={member.dataNascimento} error={errors.dataNascimento}
+                    onChange={this.handleChange}
+                    label="Data nascimento" />
+                </Col>
+                <Col>
+                  <Input name="sexo" type="text" value={member.sexo} error={errors.sexo} onChange={this.handleChange} label="Sexo" />
+                </Col>
               </Row>
               <Row>
-                <Col>{this.renderInput("cidade", "Cidade",this.handleChange)}</Col>
-                <Col>{this.renderInput("uf", "UF",this.handleChange)}</Col>
-                <Col>{this.renderInput("cep", "CEP",this.handleChange)}</Col>
+                <Col>
+                  <Input name="cidade" type="text" value={member.cidade} error={errors.cidade} onChange={this.handleChange} label="Cidade" />
+                </Col>
+                <Col>
+                  <Input name="uf" type="text" value={member.uf} error={errors.uf} onChange={this.handleChange} label="UF" />
+                </Col>
+                <Col>
+                  <Input name="cep" type="text" value={member.cep} error={errors.cep} onChange={this.handleChange} label="CEP" />
+                </Col>
               </Row>
               <Row>
-                <Col>{this.renderInput("rua", "Rua",this.handleChange)}</Col>
-                <Col>{this.renderInput("numero", "Número",this.handleChange)}</Col>
-                <Col>{this.renderInput("bairro", "Bairro",this.handleChange)}</Col>
-              </Row> */}
+                <Col>
+                  <Input name="rua" type="text" value={member.rua} error={errors.rua} onChange={this.handleChange} label="Rua" />
+                </Col>
+                <Col>
+                  <Input name="numero" type="text" value={member.numero} error={errors.numero} onChange={this.handleChange} label="Número" />
+                </Col>
+                <Col>
+                  <Input name="bairro" type="text" value={member.bairro} error={errors.bairro} onChange={this.handleChange} label="Bairro" />
+                </Col>
+              </Row>
               <Row>
-                {this.renderButton("Registrar")}
+                <Col>
+                  <button
+                    disabled={this.validateForm()}
+                    type="submit"
+                    className="btn btn-primary"
+                  >
+                    Registrar
+                  </button>
+                </Col>
               </Row>
             </form>
           </Card.Body>
@@ -150,16 +154,11 @@ class FormMember extends React.Component {
   }
 
   render() {
-    console.log(this.state);
-    // const { member } = this.state;
     return (
       <Dashboard title="Dados aluno">
-        {this.state?<h1>Ola</h1>:<h3>Cu</h3>}
-        {/* {member? this.renderMemberDisabled(member)
-          : this.renderMemberEnabled()} */}
+        {this.state !== null ? this.renderMemberEnabled() : <Spinner animation="border" variant="primary" />}
       </Dashboard>
     );
   }
-};
-
+}
 export default FormMember;
